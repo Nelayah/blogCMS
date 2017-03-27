7208f6869c0df1fc73dcda5768676208c97c03e9
Nelayah <hayalen@163.com>
2017-03-27 17:23:55 +0800

    change port

--- gunicorn.py
+++ gunicorn.py
@@ -2,7 +2,7 @@ import os
 import multiprocessing
 _home = os.path.expanduser("~")
 chdir = os.path.join(_home, 'websites', 'blogCMS')
-bind = '127.0.0.1:4002'
+bind = '127.0.0.1:8010'
 workers = multiprocessing.cpu_count() * 2 + 1
 umask = 0o022
 loglevel='warning'
